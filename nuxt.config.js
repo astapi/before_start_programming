3073adc314a58252e3daded03fba5cf47a6201da
astapi <jyunichi.k@gmail.com>
2018-10-13 19:11:08 +0900

    fix: title fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'before_start_programming',
+    title: 'プログラミングはむずかしい',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
